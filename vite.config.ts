e3c4be74798870408abae9c3bb634544fc451003
zmm <zmmwl@aliyun.com>
2026-01-17 18:26:04 +0800

    change port to 5172

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5172
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
